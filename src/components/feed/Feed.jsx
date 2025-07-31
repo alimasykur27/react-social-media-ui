3f6bc3569b5551d6ae218d8391b834da006b1062
Ahmad Ali Masykur <alimasykur6050@gmail.com>
2025-08-01 00:06:13 +0700

    [PATCH] Implement dynamic data on feed components

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,20 +1,18 @@
 import Share from "../share/Share";
 import Post from "../post/Post";
 import "./feed.css"
+import { Posts } from "../../dummyData"
 
 export default function Feed() {
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        {
+          Posts.map((p) => (
+            <Post post={p} key={p.id} />
+          ))
+        }
       </div>
     </div>
   );
